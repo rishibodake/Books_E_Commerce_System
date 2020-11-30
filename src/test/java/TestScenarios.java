169e66f2a6b2d9f6aeb03195c8efb8cdc04a3b2e
rishibodake <rishibodake@gmail.com>
2020-11-30 13:05:35 +0530

    Added:Test Case For The Searching The Book

--- src/test/java/TestScenarios.java
+++ src/test/java/TestScenarios.java
@@ -1,14 +1,20 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.HomePage;
 import pages.LoginPage;
 
 public class TestScenarios extends BaseClass {
-    @Test
+    @Test(priority = 1)
     public void loginTest(){
         String expected_titleString = "Online BookStore India, Buy Books Online, Buy Book Online India - Bookswagon.com";
         LoginPage loginPage = new LoginPage(driver);
         loginPage.login();
         Assert.assertEquals(loginPage.actual_pageTitle,expected_titleString);
+    }
 
+    @Test(priority = 2)
+    public void searchTest(){
+        HomePage homePage = new HomePage(driver);
+        homePage.search_Book();
     }
 }
