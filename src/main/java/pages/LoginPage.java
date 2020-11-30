15523a768fe84471bacfb06067b60cf397d37f66
rishibodake <rishibodake@gmail.com>
2020-11-30 12:51:38 +0530

    Added:The assert statement

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -5,17 +5,20 @@ import org.openqa.selenium.WebDriver;
 
 public class LoginPage {
     WebDriver driver;
+    public String actual_pageTitle;
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
     }
     public By email = By.id("ctl00_phBody_SignIn_txtEmail");
     public By password = By.id("ctl00_phBody_SignIn_txtPassword");
-    public  By login_button = By.id("ctl00_phBody_SignIn_btnLogin");
+    public By login_button = By.id("ctl00_phBody_SignIn_btnLogin");
+
 
     public void login(){
         driver.findElement(email).sendKeys("rishibodake20@gmail.com");
         driver.findElement(password).sendKeys("jocky1234#");
         driver.findElement(login_button).click();
+        actual_pageTitle = driver.getTitle();
     }
 }
--- src/test/java/TestScenarios.java
+++ src/test/java/TestScenarios.java
@@ -1,10 +1,14 @@
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.LoginPage;
 
 public class TestScenarios extends BaseClass {
     @Test
     public void loginTest(){
+        String expected_titleString = "Online BookStore India, Buy Books Online, Buy Book Online India - Bookswagon.com";
         LoginPage loginPage = new LoginPage(driver);
         loginPage.login();
+        Assert.assertEquals(loginPage.actual_pageTitle,expected_titleString);
+
     }
 }
