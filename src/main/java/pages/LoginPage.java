42ca32a2510a4dcfc4dfe4fc7d597d193e5f581e
rishibodake <rishibodake@gmail.com>
2020-11-30 11:18:24 +0530

    Added:LoginPage As Page Object Model

---
+++ src/main/java/pages/LoginPage.java
@@ -0,0 +1,21 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+    WebDriver driver;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+    public By email = By.id("ctl00_phBody_SignIn_txtEmail");
+    public By password = By.id("ctl00_phBody_SignIn_txtPassword");
+    public  By login_button = By.id("ctl00_phBody_SignIn_btnLogin");
+
+    public void login(){
+        driver.findElement(email).sendKeys("rishibodake20@gmail.com");
+        driver.findElement(password).sendKeys("jocky1234#");
+        driver.findElement(login_button).click();
+    }
+}
