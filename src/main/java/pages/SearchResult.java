46e50845fa1b740e2862f716c70033f5ca708965
rishibodake <rishibodake@gmail.com>
2020-11-30 15:35:04 +0530

    Added:Search Result Page As POM with Method to Buy book

---
+++ src/main/java/pages/SearchResult.java
@@ -0,0 +1,23 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class SearchResult {
+    public WebDriver driver;
+
+    public SearchResult(WebDriver driver) {
+        this.driver = driver;
+    }
+    By book = By.xpath("//a[@href='https://www.bookswagon.com/book/harry-potter/9781608870394']");
+    By buyNow = By.xpath( "#ctl00_phBody_ProductDetail_divAddtoCart > a > input");
+    By newFrame = By.xpath("//iframe[@class='cboxIframe']");
+    By placeOrder = By.id("BookCart_lvCart_imgPayment");
+
+    public void buyBook(){
+        driver.findElement(book).click();
+        driver.findElement(buyNow).click();
+        driver.switchTo().frame(driver.findElement(newFrame));
+        driver.findElement(placeOrder);
+    }
+}
