0b7f2439f55e3880c4c0d5c807576935f944c627
rishibodake <rishibodake@gmail.com>
2020-12-01 12:38:15 +0530

    Added:Check_Out Cart Page as Page Factory

---
+++ src/main/java/pages/CheckOutCart_Page.java
@@ -0,0 +1,45 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class CheckOutCart_Page {
+    WebDriver driver;
+
+    public CheckOutCart_Page(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver,this);
+    }
+    @FindBy(xpath = "//a[@class='btn-red']")
+    WebElement continueButton;
+
+    //Shipping Address
+    @FindBy(id = "ctl00_cpBody_txtNewRecipientName")
+    WebElement name;
+
+    @FindBy(id = "ctl00_cpBody_txtNewCompanyName")
+    WebElement companyName;
+
+    @FindBy(id = "ctl00_cpBody_txtNewAddress")
+    WebElement streetAddress;
+
+    @FindBy(id = "ctl00_cpBody_ddlNewState")
+    WebElement state;
+
+    @FindBy(xpath = "//option[@value='MH']" )
+    WebElement MH;
+
+    @FindBy(id = "ctl00_cpBody_txtNewCity")
+    WebElement city;
+
+    @FindBy(id = "ctl00_cpBody_txtNewPincode")
+    WebElement pinCode;
+
+    @FindBy(id = "ctl00_cpBody_txtNewMobile")
+    WebElement mobile;
+
+    @FindBy(id = "ctl00_cpBody_imgSaveNew")
+    WebElement saveAndContinue;
+}
